f82b8557000ea4255575d07b970ca72d157e4e49
swamy-pavaman <swamypenupothula@pavamanagri.com>
2025-04-01 14:28:09 +0530

    first commit

---
+++ src/main/java/com/sasyak/sasyak/SasyakApplication.java
@@ -0,0 +1,13 @@
+package com.sasyak.sasyak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SasyakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SasyakApplication.class, args);
+	}
+
+}
